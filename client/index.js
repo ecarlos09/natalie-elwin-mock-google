a4c04f08524588aa671243cb332d6b144000f15d
natbibi <58271566+natbibi@users.noreply.github.com>
2021-03-09 20:42:17 +0000

    create favicon, added hover

---
+++ client/index.js
@@ -0,0 +1,5 @@
+// // My guess is it's Javascript animation. 
+// It picks a random item from the array of labels. 
+// Then it goes into a loop, changing the label to the next item 
+// in the array until it gets to the chosen one, 
+// and using animation for the transitions.
