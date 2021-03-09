802e9f7c5b493677de3ee804ef47afe3acaf091a
Elwin <elwin.carlos09@gmail.com>
2021-03-09 15:43:34 +0000

    change result key to subject in JSON object

--- server/data.js
+++ server/data.js
@@ -1,52 +1,52 @@
 const searchResults = [
     {
         id: 1,
-        result: '20 Best Mobile Games 2020',
+        subject: '20 Best Mobile Games 2020',
         link: "https://www.esquire.com/lifestyle/g31990231/best-mobile-games-2020/"
     },
     {
         id: 2,
-        result: 'Mobile game - Wikipedia',
+        subject: 'Mobile game - Wikipedia',
         link: "https://en.wikipedia.org/wiki/Mobile_game"
     },
     {
         id: 3,
-        result: 'Best gaming phone 2021',
+        subject: 'Best gaming phone 2021',
         link: "https://www.techradar.com/uk/news/best-phone-for-gaming"
     },
     {
         id: 4,
-        result: 'Mobile gaming is a $68.5 billion global business',
+        subject: 'Mobile gaming is a $68.5 billion global business',
         link: "https://techcrunch.com/2019/08/22/mobile-gaming-mints-money/?guccounter=1&guce_referrer=aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8&guce_referrer_sig=AQAAAIbmD3amgpxGQemKif4Ywt51WtgggLVAxIsX4yZx-lMpTRvd9PUs_LxR5VtHS9nk1wCXtIkr1hltUwiwbf8WXsSPUprwsE0i76vd1Qm7nKZqAIpsj7vnXmka_s_S78UlZmHDXVeEJZW--BBYxqtISE4kVWiP2hXa7_9ok3cXLss_"
     },
     {
         id: 5,
-        result: 'Best mobile games of 2019',
+        subject: 'Best mobile games of 2019',
         link: "https://www.cnet.com/pictures/best-mobile-games-2019/"
     },
     {
         id: 6,
-        result: 'Pocket Gamer: The world\'s number one mobile...',
+        subject: 'Pocket Gamer: The world\'s number one mobile...',
         link: "https://www.pocketgamer.com/"
     },
     {
         id: 7,
-        result: 'Mobile gaming - Statistics & Facts',
+        subject: 'Mobile gaming - Statistics & Facts',
         link: "https://www.statista.com/topics/1906/mobile-gaming/"
     },
     {
         id: 8,
-        result: 'Mobile gaming is on the rise...',
+        subject: 'Mobile gaming is on the rise...',
         link: "https://www.theverge.com/2020/5/27/21270971/mobile-gaming-record-woodturning-ludo-king-coronavirus"
     },
     {
         id: 9,
-        result: 'Seamless Mobile Gaming- sony.co.uk',
+        subject: 'Seamless Mobile Gaming- sony.co.uk',
         link: "https://www.sony.co.uk/electronics/smartphones/xperia-5m2?cid=sem-eu-111224773"
     },
     {
         id: 10,
-        result: 'Video Game Mobile Kids Party - Book a Gaming Party From £189',
+        subject: 'Video Game Mobile Kids Party - Book a Gaming Party From £189',
         link: "https://www.popuparcade.co.uk/?gclid=Cj0KCQiA1pyCBhCtARIsAHaY_5f5LgZZBLwg7hR-wROVVJXsg4f4HMKMqHGQ4i4MV6is-lYf_Y9zwSQaAgw6EALw_wcB"
     },
 ];
