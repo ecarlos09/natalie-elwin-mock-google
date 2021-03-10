641ef36480a5d4fb7f9dab9d9e929e518013e271
Elwin <76127821+ecarlos09@users.noreply.github.com>
2021-03-10 13:59:44 +0000

    Update results.js

--- server/controllers/results.js
+++ server/controllers/results.js
@@ -7,13 +7,6 @@ const Result = require('../models/result');
 
 //Retrieve all results from data.js
 router.get('/', (req, res) => {
-    // try {
-    //     const resultsData = Result.all;
-    //     console.log(res.send(resultsData));
-    // } catch (err) {
-    //     console.log(err);
-    //     res.status(404).send(err);
-    // };
     const resultsData = Result.all;
     res.send(resultsData);
 });
