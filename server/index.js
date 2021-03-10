f96e7e122b96581870bca04dd40f4af1e008b804
Elwin <76127821+ecarlos09@users.noreply.github.com>
2021-03-10 14:23:37 +0000

    Delete index.js

--- server/index.js
+++
@@ -1,6 +0,0 @@
-//Import server
-const app = require('./app');
-//Define local host for API server
-const port = 3000;
-//Set up request listener
-app.listen(port, () => console.log(`Visit http://localhost:${port} to see our content!`));
