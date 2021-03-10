c3fac857a77ed8e235f1502ea8790419a3717bdf
natbibi <58271566+natbibi@users.noreply.github.com>
2021-03-10 10:42:18 +0000

    natalie's morning changes

--- client/index.js
+++ client/index.js
@@ -3,3 +3,25 @@
 // Then it goes into a loop, changing the label to the next item 
 // in the array until it gets to the chosen one, 
 // and using animation for the transitions.
+
+
+function init() {
+    const form = document.getElementById('sub-btn');
+    form.addEventListener('submit', e => { returnSearch(e) })
+}
+
+// init()
+
+function returnSearch(e) {
+    e.preventDefault();
+    fetch('http://localhost:3000/results')
+    .then(res => res.json())
+    .then(addResultsToPage)
+    .catch(err => console.warn('OH NO, something went wrong!', err))  
+
+function addResultsToPage(data) {
+    document.getElementsByClassName('container').remove()
+    
+}
+    id = "lucky-btn"
+    id = "sub-btn"
